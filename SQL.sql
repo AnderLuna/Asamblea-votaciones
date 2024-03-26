74db4bf2dab0e07699e60e805c4a1a1faedc23fe
Lunay <andersonluna704@gmail.com>
2024-03-25 23:52:59 -0500

    Create database.php

---
+++ SQL.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Usuarios (
+    ID VARCHAR(10) PRIMARY KEY NOT NULL,
+    Nombre VARCHAR(100) NOT NULL,
+    Apellido VARCHAR(100) NOT NULL,
+    Edad INT NOT NULL,
+    Cargo VARCHAR(100) NOT NULL,
+    Email VARCHAR(100),
+    Usuario VARCHAR(100) NOT NULL,
+    Contraseña VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Propuestas (
+    IDPropuesta INT PRIMARY KEY NOT NULL,
+    Titulo VARCHAR(100) NOT NULL,
+    Descripcion VARCHAR(255) NOT NULL,
+    IDUsuario VARCHAR(10) NOT NULL,
+    FOREIGN KEY (IDUsuario) REFERENCES Usuarios(ID)
+);
+
+CREATE TABLE Votaciones (
+    IDPropuesta INT NOT NULL,
+    IDUsuario VARCHAR(10) NOT NULL,
+    Voto VARCHAR(50) NOT NULL,
+    PRIMARY KEY (IDPropuesta, IDUsuario),
+    FOREIGN KEY (IDPropuesta) REFERENCES Propuestas(IDPropuesta),
+    FOREIGN KEY (IDUsuario) REFERENCES Usuarios(ID)
+);
---
+++ validaciones.php
@@ -0,0 +1,43 @@
+<?php
+require_once 'database.php';
+
+// Verificar si se han enviado datos de usuario y contraseña y si no están vacíos
+if (!empty($_POST["username"]) && !empty($_POST["password"])) {
+    // Capturar los datos del formulario
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // Validar los datos de usuario y contraseña (puedes implementar tus propias validaciones aquí)
+
+    // Obtener la conexión a la base de datos
+    $conexion = Database::obtenerConexion();
+
+    // Consulta SQL para buscar al usuario en la base de datos
+    $query = "SELECT * FROM usuarios WHERE usuario = '$username' AND contraseña = '$password'";
+
+    // Ejecutar la consulta
+    $resultado = $conexion->query($query);
+
+    // Verificar si se encontró algún resultado
+    if ($resultado->num_rows > 0) {
+        // El usuario y la contraseña son válidos
+        echo "Login exitoso. Bienvenido, $username!";
+        
+        // Verificar si el usuario y la contraseña son "admin" y "admon"
+        if ($username == "admon" && $password == "admon") {
+            // Redireccionar a admon.html
+            header("Location: admon.html");
+            exit(); // Asegurar que el script se detenga después de redirigir
+        }
+    } else {
+        // El usuario o la contraseña son incorrectos
+        echo "Usuario o contraseña incorrectos.";
+    }
+
+    // Cerrar la conexión a la base de datos
+    $conexion->close();
+} else {
+    // Si los datos están vacíos, mostrar un mensaje de error
+    echo "Por favor, ingresa tanto el nombre de usuario como la contraseña.";
+}
+?>
