bf31ec8e8c064ec12a19a0d18163d8bb6222b3b9
Lunay <andersonluna704@gmail.com>
2024-03-27 19:46:14 -0500

    Direcionamiento de datos a la seccion de administrador
    Se agergo logica para direccionar los datos capturados en la base de datos y mandarlas a la seccion de administrador.

--- validaciones_login.php
+++ validaciones_login.php
@@ -28,7 +28,7 @@ if (!empty($_POST["username"]) && !empty($_POST["password"])) {
         // Verificar si el usuario y la contraseña son "admin" y "admon"
         if ($username == "admon" && $password == "admon") {
             // Redireccionar a admon.html
-            header("Location: admon.html");
+            header("Location: admon.php?nombre={$usuario['nombre']}&apellido={$usuario['apellido']}");
             exit(); // Asegurar que el script se detenga después de redirigir
         }else{
             header("Location: usuario.php?id={$usuario['id']}&nombre={$usuario['nombre']}&apellido={$usuario['apellido']}");
