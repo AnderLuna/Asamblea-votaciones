e50aedc8931c326eeaaf6226be58edcded47e3e9
Lunay <andersonluna704@gmail.com>
2024-03-28 23:59:35 -0500

    Update registrar_propuesta.php

--- registrar_propuesta.php
+++ registrar_propuesta.php
@@ -11,12 +11,10 @@ if (!empty($_POST["titulo"]) && !empty($_POST["descripcion"])) {
     $conexion = Database::obtenerConexion(); 
 
     // Consulta SQL para insertar la propuesta en la base de datos
-    $query = "INSERT INTO propuestas (idpropuesta, titulo, descripcion, idusuario) VALUES ('$IDPropuesta', '$titulo', '$descripcion', '$id')";
-
-    $sql = "INSERT INTO votaciones (idpropuesta, idusuario, votos) VALUES ('$IDPropuesta', '$id', 0)";
+    $query = "INSERT INTO propuestas (idpropuesta, titulo, descripcion, idusuario, votos) VALUES ('$IDPropuesta', '$titulo', '$descripcion', '$id', 0)";
 
     // Ejecutar la consulta SQL y manejar los errores
-    if ($conexion->query($query) === TRUE and $conexion->query($sql) === TRUE) {
+    if ($conexion->query($query) === TRUE) {
         echo "Propuesta registrada correctamente.";
     } else {
         echo "Error al registrar la propuesta: " . $conexion->error;
