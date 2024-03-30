ad35c37913aba1ae2671c44b15c88a519d2461ac
Lunay <andersonluna704@gmail.com>
2024-03-30 14:47:53 -0500

    Create encabezado.php

---
+++ encabezado.php
@@ -0,0 +1,26 @@
+<?php
+    require_once 'database.php';
+
+    // Recoger los datos del usuario de la URL
+    $id = $_GET['id'];
+    $conexion = Database::obtenerConexion();
+
+    // Consulta SQL para buscar al usuario en la base de datos
+    $query = "SELECT nombre, apellido FROM usuarios WHERE id='$id'";
+
+    // Ejecutar la consulta
+    $resultado = $conexion->query($query);
+
+    // Verificar si se encontró algún resultado
+    if ($resultado->num_rows > 0) {        
+
+        $usuario = $resultado->fetch_assoc();
+        $nombre=$usuario['nombre'];
+        $apellido=$usuario['apellido'];
+
+        echo "<h1>Bienvenido, $id - $nombre - $apellido</h1>";
+
+    }     
+
+    
+?>
