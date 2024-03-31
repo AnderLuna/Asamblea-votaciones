a34ff5d3ef5ced6fd984d4135be67d8c8a5649e0
PineAlpina <miguelpime85@gmail.com>
2024-03-31 14:28:08 -0500

    Update visualizacion_propuesta.php

--- visualizacion_propuesta.php
+++ visualizacion_propuesta.php
@@ -6,7 +6,7 @@ require_once 'database.php';
 $conexion = Database::obtenerConexion();
 
 // Consulta SQL para obtener las propuestas
-$query = "SELECT * FROM `propuestas` WHERE 1";
+$query = "SELECT idpropuesta, titulo, descripcion, votos FROM usuarios, propuestas WHERE usuarios.id = propuestas.idusuario and id = '$id'";
 
 $resultado = $conexion->query($query);
 
