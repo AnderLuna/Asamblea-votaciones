f52303992e8002a9a31d02994acaaa411ed03054
Lunay <andersonluna704@gmail.com>
2024-04-01 15:44:03 -0500

    Update usuario.php

--- usuario.php
+++ usuario.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="usuario.css">    
+    <link rel="stylesheet" href="styles/usuario.css">    
     <title>Panel de Usuario</title>
 </head>
 
