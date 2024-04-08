ebab35265659ded9f4a82cefde220a79ed23317e
Lunay <andersonluna704@gmail.com>
2024-04-07 21:58:43 -0500

    Update votaciones.php

--- votaciones.php
+++ votaciones.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/usuario.css">    
+    <link rel="stylesheet" href="styles/usuario.css">
     <title>Panel de Votaciones</title>
 </head>
-
 <body>
 
     <div class="container">
