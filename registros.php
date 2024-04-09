18f48a44df050d507b4ba364f7fb457a53f50872
JhonatanCuero <162332531+JhonatanCuero@users.noreply.github.com>
2024-04-09 12:33:31 -0500

    Actualización de validaciones
    Se añadieron las validaciones que faltaban de nombre, apellido, cargo y correo.

--- registros.php
+++ registros.php
@@ -2,17 +2,18 @@
 require_once 'database.php';
 
     // Capturar los datos del formulario
-    $id = $_POST["id"];
-    $nombre = $_POST["nombre"];
-    $apellido = $_POST["apellido"];
-    $edad = $_POST["edad"];
-    $cargo = $_POST["cargo"];
-    $email = $_POST["email"];
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+    $id = trim($_POST["id"]); //Trim: elimina los espacios vacios al inicio y final de una cadena de txt
+    $nombre = trim($_POST["nombre"]);
+    $apellido = trim($_POST["apellido"]);
+    $edad = trim($_POST["edad"]);
+    $cargo = trim($_POST["cargo"]);
+    $email = trim($_POST["email"]);
+    $username = trim($_POST["username"]);
+    $password = trim($_POST["password"]);
+    
 
 // Verificar si se han enviado datos del formulario de registro
-if (validarID($id) === true){
+if (validarID($id) && validarCampos($nombre, $apellido, $cargo) && validarEmail($email)){
 
     // Obtener la conexión a la base de datos
     $conexion = Database::obtenerConexion();
@@ -58,6 +59,38 @@ function validarID($ID) {
     } else {
         echo "El ID debe ser una cadena de 10 dígitos.";
         return false; // La cadena no tiene exactamente 10 números
+    }
+}
+
+function validarCampos($nombre, $apellido, $cargo) {
+    // Verificar si alguno de los campos está en blanco
+    if (empty($nombre) || empty($apellido) || empty($cargo)) {
+        echo "Los campos: nombre, apellido y cargo NO pueden estar vacios.";
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validarEmail($correo) {
+
+    // Verificar si el correo está vacío 
+    if (empty($correo)) {
+        echo "El correo electrónico está vacío.";
+        return false;
+    }
+    // Verificar si el formato del correo electrónico es válido
+    if (filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+        // Verificar si el correo electrónico contiene caracteres especiales
+        if (preg_match('/[\'^£$%&*()}{#~?><>,|=+¬]/', $correo)) {
+            echo "El correo electrónico no puede contener caracteres especiales.";
+            return false;
+        } else {
+            return true; // El correo electrónico es válido y no contiene caracteres especiales
+        }
+    } else {
+        echo  "El formato del correo electrónico es inválido.";
+        return false;
     }
 }
 ?>
