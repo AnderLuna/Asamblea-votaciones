e64fae47e8e48928e1e14ffb46fb8253dee2c0aa
Lunay <andersonluna704@gmail.com>
2024-04-08 23:48:40 -0500

    Update asamblea_actual.php

--- asamblea_actual.php
+++ asamblea_actual.php
@@ -6,7 +6,7 @@
     $conexion = Database::obtenerConexion();
 
     // Consulta SQL para buscar al usuario en la base de datos
-    $query = "SELECT tema FROM asambleas WHERE estado='activa'";
+    $query = "SELECT idasamblea, tema FROM asambleas WHERE estado='activa'";
 
     // Ejecutar la consulta
     $resultado = $conexion->query($query);
@@ -15,9 +15,10 @@
     if ($resultado->num_rows > 0) {        
 
         $data = $resultado->fetch_assoc();
+        $idasamblea=$data['idasamblea'];
         $tema=$data['tema'];
 
-        echo "<div class='encabezado'>TEMA DE LA ASAMBLEA:, <span>$tema</span></div>";
+        echo "<div class='encabezado'>ASAMBLEA ACTUAL <br> ID: <span>$idasamblea</span> - TEMA: <span>$tema</span></div>";
 
 
 
