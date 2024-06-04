e2af5b3a0554c6fd0283bd90eafecf92e235b02d
AnderLuna <andersonluna704@gmail.com>
2024-06-04 07:30:22 -0500

    Update visualizacion_asambleas_pasadas.php

--- visualizacion_asambleas_pasadas.php
+++ visualizacion_asambleas_pasadas.php
@@ -5,8 +5,25 @@ require_once 'database.php';
 // Obtener la conexión a la base de datos
 $conexion = Database::obtenerConexion();
 
-// Consulta SQL para obtener el total de votos por asamblea
-$query = "SELECT asambleas.idasamblea, asambleas.tema, asambleas.fecha, asambleas.estado, SUM(propuestas.votos) AS total_votos FROM asambleas, propuestas WHERE asambleas.idasamblea=propuestas.idasamblea and asambleas.estado = 'cerrada' GROUP BY asambleas.idasamblea";
+// Consulta SQL para obtener el nombre de la asamblea, el tema y la propuesta con más votos del tema
+$query = "
+SELECT 
+    asambleas.nombre AS nombre_asamblea, 
+    subtemas.tema AS nombre_tema, 
+    propuestas.descripcion AS propuesta_con_mas_votos,
+    MAX(propuestas.votos) AS max_votos
+FROM 
+    asambleas
+JOIN 
+    subtemas ON asambleas.idasamblea = subtemas.idasamblea
+JOIN 
+    propuestas ON subtemas.idtema = propuestas.idtema
+WHERE 
+    asambleas.estado = 'cerrada'
+GROUP BY 
+    asambleas.nombre, subtemas.tema
+ORDER BY 
+    asambleas.nombre, subtemas.tema";
 
 $resultado = $conexion->query($query);
 
@@ -15,16 +32,16 @@ if ($resultado->num_rows > 0) {
     // Mostrar los datos en la tabla
     while ($fila = $resultado->fetch_assoc()) {
         echo "<tr>";
-        echo "<td>" . $fila['idasamblea'] . "</td>";
-        echo "<td>" . $fila['tema'] . "</td>";
-        echo "<td>" . $fila['fecha'] . "</td>";
-        echo "<td>" . $fila['estado'] . "</td>";
-        echo "<td>" . $fila['total_votos'] . "</td>";
+        echo "<td>" . $fila['nombre_asamblea'] . "</td>";
+        echo "<td>" . $fila['nombre_tema'] . "</td>";
+        echo "<td>" . $fila['propuesta_con_mas_votos'] . "</td>";
+        echo "<td>" . $fila['max_votos'] . "</td>";
         echo "</tr>";
+        echo "<tr><td colspan='3'>-----------------------------</td></tr>";
     }
 } else {
-    // Si no se encontraron asambleas disponibles, mostrar un mensaje en la tabla
-    echo "<tr><td colspan='5'>No se encontraron asambleas o propuestas.</td></tr>";
+    // Si no se encontraron asambleas o propuestas, mostrar un mensaje en la tabla
+    echo "<tr><td colspan='3'>No se encontraron asambleas o propuestas.</td></tr>";
 }
 
 // Cerrar la conexión a la base de datos
